6d22058f5403d3aa706a048668557f28e0125a67
bouthaina-hrour <73646814+bouthaina-hrour@users.noreply.github.com>
2023-02-06 00:43:58 +0100

    first commit

--- projects/script.js
+++ projects/script.js
@@ -21,11 +21,11 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Hrour bouthaina";
-            $("#favicon").attr("href", "/assets/images/favicon.png");
+            $("#favicon").attr("href", "/assets/images/me.png");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "/assets/images/favhand.png");
+            $("#favicon").attr("href", "/assets/images/me.png");
         }
     });
 
